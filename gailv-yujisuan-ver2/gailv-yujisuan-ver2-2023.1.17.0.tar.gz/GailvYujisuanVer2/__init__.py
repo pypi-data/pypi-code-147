1968a3fbe2ce8b7eb2da90390d931a2b714cdc8c
Tom Forbes <tom@tomforb.es>
2023-01-17 06:42:03 +0000

    {"name":"gailv-yujisuan-ver2","version":"2023.1.17.0","file":"gailv-yujisuan-ver2-2023.1.17.0.tar.gz","path":"gailv-yujisuan-ver2/gailv-yujisuan-ver2-2023.1.17.0.tar.gz"}

---
+++ gailv-yujisuan-ver2/gailv-yujisuan-ver2-2023.1.17.0.tar.gz/GailvYujisuanVer2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gailv-yujisuan-ver2
+https://github.com/apachecn/gailv-yujisuan-ver2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
