3f14e7f28e7a9f9065d04a53314da5f8d71c0273
Tom Forbes <tom@tomforb.es>
2023-01-17 06:38:05 +0000

    {"name":"duoyuan-shijian-xulie-fenxi-jijinrong-yingyong","version":"2023.1.17.0","file":"duoyuan_shijian_xulie_fenxi_jijinrong_yingyong-2023.1.17.0-py3-none-any.whl","path":"duoyuan-shijian-xulie-fenxi-jijinrong-yingyong/duoyuan_shijian_xulie_fenxi_jijinrong_yingyong-2023.1.17.0-py3-none-any.whl"}

---
+++ duoyuan-shijian-xulie-fenxi-jijinrong-yingyong/duoyuan_shijian_xulie_fenxi_jijinrong_yingyong-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""duoyuan-shijian-xulie-fenxi-jijinrong-yingyong
+https://github.com/apachecn/duoyuan-shijian-xulie-fenxi-jijinrong-yingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
