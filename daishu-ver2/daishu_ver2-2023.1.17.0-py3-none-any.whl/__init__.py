d3c70ba2f318324877c7cfc08da60e8c60acdc98
Tom Forbes <tom@tomforb.es>
2023-01-17 06:36:38 +0000

    {"name":"daishu-ver2","version":"2023.1.17.0","file":"daishu_ver2-2023.1.17.0-py3-none-any.whl","path":"daishu-ver2/daishu_ver2-2023.1.17.0-py3-none-any.whl"}

---
+++ daishu-ver2/daishu_ver2-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daishu-ver2
+https://github.com/apachecn/daishu-ver2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
