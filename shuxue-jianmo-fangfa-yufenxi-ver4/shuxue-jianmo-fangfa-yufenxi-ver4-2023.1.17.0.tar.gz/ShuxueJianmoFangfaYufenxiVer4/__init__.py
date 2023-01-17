7715c274eac63883cfdf11b78ace52281ad9dfec
Tom Forbes <tom@tomforb.es>
2023-01-17 07:01:21 +0000

    {"name":"shuxue-jianmo-fangfa-yufenxi-ver4","version":"2023.1.17.0","file":"shuxue-jianmo-fangfa-yufenxi-ver4-2023.1.17.0.tar.gz","path":"shuxue-jianmo-fangfa-yufenxi-ver4/shuxue-jianmo-fangfa-yufenxi-ver4-2023.1.17.0.tar.gz"}

---
+++ shuxue-jianmo-fangfa-yufenxi-ver4/shuxue-jianmo-fangfa-yufenxi-ver4-2023.1.17.0.tar.gz/ShuxueJianmoFangfaYufenxiVer4/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuxue-jianmo-fangfa-yufenxi-ver4
+https://github.com/apachecn/shuxue-jianmo-fangfa-yufenxi-ver4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
