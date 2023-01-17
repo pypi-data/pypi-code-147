18a7257b5cbc1d447c0c7c13482d2776fde0f66b
Tom Forbes <tom@tomforb.es>
2023-01-17 06:40:34 +0000

    {"name":"fufenxi-jichu-jigongcheng-yingyong-ver3","version":"2023.1.17.0","file":"fufenxi_jichu_jigongcheng_yingyong_ver3-2023.1.17.0-py3-none-any.whl","path":"fufenxi-jichu-jigongcheng-yingyong-ver3/fufenxi_jichu_jigongcheng_yingyong_ver3-2023.1.17.0-py3-none-any.whl"}

---
+++ fufenxi-jichu-jigongcheng-yingyong-ver3/fufenxi_jichu_jigongcheng_yingyong_ver3-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fufenxi-jichu-jigongcheng-yingyong-ver3
+https://github.com/apachecn/fufenxi-jichu-jigongcheng-yingyong-ver3"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
