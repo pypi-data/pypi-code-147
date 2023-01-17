3210638949922a46b1d3fe2330fb3261db607e81
Tom Forbes <tom@tomforb.es>
2023-01-17 07:15:45 +0000

    {"name":"xianxing-daishu-jiqi-yingyong-ver4","version":"2023.1.17.0","file":"xianxing_daishu_jiqi_yingyong_ver4-2023.1.17.0-py3-none-any.whl","path":"xianxing-daishu-jiqi-yingyong-ver4/xianxing_daishu_jiqi_yingyong_ver4-2023.1.17.0-py3-none-any.whl"}

---
+++ xianxing-daishu-jiqi-yingyong-ver4/xianxing_daishu_jiqi_yingyong_ver4-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xianxing-daishu-jiqi-yingyong-ver4
+https://github.com/apachecn/xianxing-daishu-jiqi-yingyong-ver4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
