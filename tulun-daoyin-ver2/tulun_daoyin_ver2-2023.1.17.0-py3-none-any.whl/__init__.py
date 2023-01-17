612927e9ea87048ef1c0db81da70272d244f2ab2
Tom Forbes <tom@tomforb.es>
2023-01-17 07:06:28 +0000

    {"name":"tulun-daoyin-ver2","version":"2023.1.17.0","file":"tulun_daoyin_ver2-2023.1.17.0-py3-none-any.whl","path":"tulun-daoyin-ver2/tulun_daoyin_ver2-2023.1.17.0-py3-none-any.whl"}

---
+++ tulun-daoyin-ver2/tulun_daoyin_ver2-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tulun-daoyin-ver2
+https://github.com/apachecn/tulun-daoyin-ver2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
