68e4889c7a21ff0b5498338e07888f62d253beff
Tom Forbes <tom@tomforb.es>
2023-01-17 06:45:16 +0000

    {"name":"jinrong-shijian-xulie-fenxi","version":"2023.1.17.0","file":"jinrong_shijian_xulie_fenxi-2023.1.17.0-py3-none-any.whl","path":"jinrong-shijian-xulie-fenxi/jinrong_shijian_xulie_fenxi-2023.1.17.0-py3-none-any.whl"}

---
+++ jinrong-shijian-xulie-fenxi/jinrong_shijian_xulie_fenxi-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jinrong-shijian-xulie-fenxi
+https://github.com/apachecn/jinrong-shijian-xulie-fenxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
