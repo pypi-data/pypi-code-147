3a57f5e86b4d33f268a69ef313b94ba20c776d28
Tom Forbes <tom@tomforb.es>
2023-01-17 07:23:20 +0000

    {"name":"yingyong-zuhe-shuxue-ver2","version":"2023.1.17.0","file":"yingyong-zuhe-shuxue-ver2-2023.1.17.0.tar.gz","path":"yingyong-zuhe-shuxue-ver2/yingyong-zuhe-shuxue-ver2-2023.1.17.0.tar.gz"}

---
+++ yingyong-zuhe-shuxue-ver2/yingyong-zuhe-shuxue-ver2-2023.1.17.0.tar.gz/YingyongZuheShuxueVer2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingyong-zuhe-shuxue-ver2
+https://github.com/apachecn/yingyong-zuhe-shuxue-ver2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
