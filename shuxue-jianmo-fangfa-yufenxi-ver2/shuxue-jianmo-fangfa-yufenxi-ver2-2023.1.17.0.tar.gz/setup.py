2af97a52c096cad91ebeb9c55129540700aa185c
Tom Forbes <tom@tomforb.es>
2023-01-17 07:00:02 +0000

    {"name":"shuxue-jianmo-fangfa-yufenxi-ver2","version":"2023.1.17.0","file":"shuxue-jianmo-fangfa-yufenxi-ver2-2023.1.17.0.tar.gz","path":"shuxue-jianmo-fangfa-yufenxi-ver2/shuxue-jianmo-fangfa-yufenxi-ver2-2023.1.17.0.tar.gz"}

---
+++ shuxue-jianmo-fangfa-yufenxi-ver2/shuxue-jianmo-fangfa-yufenxi-ver2-2023.1.17.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ShuxueJianmoFangfaYufenxiVer2
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ShuxueJianmoFangfaYufenxiVer2'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="shuxue-jianmo-fangfa-yufenxi-ver2",
+    version=ShuxueJianmoFangfaYufenxiVer2.__version__,
+    url="https://github.com/apachecn/shuxue-jianmo-fangfa-yufenxi-ver2",
+    author=ShuxueJianmoFangfaYufenxiVer2.__author__,
+    author_email=ShuxueJianmoFangfaYufenxiVer2.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="09-数学建模方法与分析原书第2版",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "shuxue-jianmo-fangfa-yufenxi-ver2=ShuxueJianmoFangfaYufenxiVer2.__main__:main",
+            "ShuxueJianmoFangfaYufenxiVer2=ShuxueJianmoFangfaYufenxiVer2.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
