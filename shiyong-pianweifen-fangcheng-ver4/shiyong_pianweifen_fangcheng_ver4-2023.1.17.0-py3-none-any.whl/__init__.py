6c736f81929880474b7be10a9f5e6f854f38c152
Tom Forbes <tom@tomforb.es>
2023-01-17 06:55:42 +0000

    {"name":"shiyong-pianweifen-fangcheng-ver4","version":"2023.1.17.0","file":"shiyong_pianweifen_fangcheng_ver4-2023.1.17.0-py3-none-any.whl","path":"shiyong-pianweifen-fangcheng-ver4/shiyong_pianweifen_fangcheng_ver4-2023.1.17.0-py3-none-any.whl"}

---
+++ shiyong-pianweifen-fangcheng-ver4/shiyong_pianweifen_fangcheng_ver4-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shiyong-pianweifen-fangcheng-ver4
+https://github.com/apachecn/shiyong-pianweifen-fangcheng-ver4"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
