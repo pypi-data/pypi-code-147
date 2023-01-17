8aaa334c4bf9d0df72083230e2d9f970ecbb5790
Tom Forbes <tom@tomforb.es>
2023-01-17 06:54:00 +0000

    {"name":"shijian-xulie-fenxi-jiyingyong","version":"2023.1.17.0","file":"shijian_xulie_fenxi_jiyingyong-2023.1.17.0-py3-none-any.whl","path":"shijian-xulie-fenxi-jiyingyong/shijian_xulie_fenxi_jiyingyong-2023.1.17.0-py3-none-any.whl"}

---
+++ shijian-xulie-fenxi-jiyingyong/shijian_xulie_fenxi_jiyingyong-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shijian-xulie-fenxi-jiyingyong
+https://github.com/apachecn/shijian-xulie-fenxi-jiyingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
