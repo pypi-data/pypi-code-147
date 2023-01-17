fd73b8ff28660286693f0bf6bb27f697598ecd0e
Tom Forbes <tom@tomforb.es>
2023-01-17 07:03:42 +0000

    {"name":"shuzhi-fangfa-sheji-fenxi-hesuanfa-shixian","version":"2023.1.17.0","file":"shuzhi_fangfa_sheji_fenxi_hesuanfa_shixian-2023.1.17.0-py3-none-any.whl","path":"shuzhi-fangfa-sheji-fenxi-hesuanfa-shixian/shuzhi_fangfa_sheji_fenxi_hesuanfa_shixian-2023.1.17.0-py3-none-any.whl"}

---
+++ shuzhi-fangfa-sheji-fenxi-hesuanfa-shixian/shuzhi_fangfa_sheji_fenxi_hesuanfa_shixian-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuzhi-fangfa-sheji-fenxi-hesuanfa-shixian
+https://github.com/apachecn/shuzhi-fangfa-sheji-fenxi-hesuanfa-shixian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
