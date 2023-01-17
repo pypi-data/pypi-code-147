20c6c117e2c30e6815fe5a1cd145b16d650bd9f5
Tom Forbes <tom@tomforb.es>
2023-01-17 06:52:43 +0000

    {"name":"shifenxi-yufufenxi-ver3","version":"2023.1.17.0","file":"shifenxi_yufufenxi_ver3-2023.1.17.0-py3-none-any.whl","path":"shifenxi-yufufenxi-ver3/shifenxi_yufufenxi_ver3-2023.1.17.0-py3-none-any.whl"}

---
+++ shifenxi-yufufenxi-ver3/shifenxi_yufufenxi_ver3-2023.1.17.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shifenxi-yufufenxi-ver3
+https://github.com/apachecn/shifenxi-yufufenxi-ver3"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2023.1.17.0"
